9cb79ee3ea5acb3e72c5196599e9a0b56f4b4037
ayunas <ayunas@gmail.com>
2019-05-16 14:50:15 -0500

    after creating a table in the database using a migration and seeding it with 3 initial values, created all the endpoints for the CRUD operations.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const server = require('./server')
+const server = require('./server');
 require('dotenv').config();
 
-
 const port = process.env.PORT || 5500
+
 
 server.listen(port, () => {
     console.log(`\n***Server listening on port ${port}`)
